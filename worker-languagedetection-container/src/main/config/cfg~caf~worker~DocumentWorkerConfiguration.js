02fec9682e331cbf89b0970f80066f618bca39a0
rado-straka <35526049+rado-straka@users.noreply.github.com>
2018-04-04 16:17:46 +0100

    SCMOD-3559: Update worker configs for all document workers in enrichm… (#15)
    * SCMOD-3559: Update worker configs for all document workers in enrichment workflow.
    
    * SCMOD-3559: Revert release notes change.
    
    * SCMOD-3559: Updated documentation
    
    * SCMOD-3559: Update documentation.
    
    * Change 'static' to 'dynamic' in env description

--- worker-languagedetection-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
+++ worker-languagedetection-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
@@ -20,5 +20,15 @@
             || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-out",
     threads: getenv("CAF_WORKER_THREADS") || 1,
     maxBatchSize: getenv("CAF_WORKER_MAX_BATCH_SIZE") || undefined,
-    maxBatchTime: getenv("CAF_WORKER_MAX_BATCH_TIME") || undefined
+    maxBatchTime: getenv("CAF_WORKER_MAX_BATCH_TIME") || undefined,
+    scriptCaching: {
+        staticScriptCache: {
+            maximumSize: getenv("CAF_WORKER_STATIC_SCRIPT_CACHE_SIZE") || undefined,
+            expireAfterAccess: getenv("CAF_WORKER_STATIC_SCRIPT_CACHE_DURATION") || undefined
+        },
+        dynamicScriptCache: {
+            maximumSize: getenv("CAF_WORKER_DYNAMIC_SCRIPT_CACHE_SIZE") || undefined,
+            expireAfterWrite: getenv("CAF_WORKER_DYNAMIC_SCRIPT_CACHE_DURATION") || undefined
+        }
+    }
 });
