45c4459b526c9048d994ae2253c22619d82f8f5c
David Milligan <dmilligan@opentext.com>
2025-08-13 11:09:57 +0100

    Removed invalid queue cfg, added missing cfg

--- worker-languagedetection-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
+++ worker-languagedetection-container/src/main/config/cfg~caf~worker~DocumentWorkerConfiguration.js
@@ -18,8 +18,6 @@
     workerVersion: "${project.version}",
     outputQueue: getenv("CAF_WORKER_OUTPUT_QUEUE")
             || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-out",
-    invalidQueue: getenv("CAF_WORKER_INVALID_QUEUE")
-        || (getenv("CAF_WORKER_BASE_QUEUE_NAME") || getenv("CAF_WORKER_NAME") || "worker") + "-invalid",
     threads: getenv("CAF_WORKER_THREADS") || 1,
     maxBatchSize: getenv("CAF_WORKER_MAX_BATCH_SIZE") || undefined,
     maxBatchTime: getenv("CAF_WORKER_MAX_BATCH_TIME") || undefined,
